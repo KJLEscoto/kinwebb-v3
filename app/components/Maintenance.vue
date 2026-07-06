cbd94b60d3d86f96f35a00bfed7a689ea0a71e6f
Kent Joemar Escoto <kentescoto24@gmail.com>
2026-07-06 20:08:19 +0800

    added tailwind and partial maintenance page

---
+++ app/components/Maintenance.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative flex items-end justify-start min-h-screen px-20 py-10 overflow-hidden">
+    <img src="/images/hero_image.jpg" alt="Hero Image"
+      class="w-full object-cover min-h-auto h-screen absolute inset-0 select-none" draggable="false" />
+    <p class="font-secondary italic text-4xl absolute top-[40%] left-[35%]">Hey!..</p>
+    <div class="relative z-10 space-y-5">
+      <h1 class="text-9xl font-tertiary text-secondary">I’ll Be Back Soon! <span
+          class="text-3xl font-secondary italic">I Promise...</span></h1>
+      <p class="w-1/2 text-xl">
+        It’s just I am currently working on a fresh new version of this website... with better content, cleaner visuals,
+        and updated project showcases.
+      </p>
+      <section class="flex items-center gap-3">
+        <button class="rounded-full bg-white px-8 py-3">
+          <p class="font-semibold text-primary">LET'S CHAT</p>
+        </button>
+        <button class="px-8 py-3 border border-white/20 rounded-full">
+          <p class="font-semibold text-white">MY OLD WEBSITE</p>
+        </button>
+      </section>
+    </div>
+  </div>
+</template>
---
+++ app/components/UI/Loader.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p class="text-2xl font-black font-primary text-primary">
+      Loader
+    </p>
+  </div>
+</template>
